4c1134373c1986b796afef2aef2451dcea424c93
Charles Abasimfon <devabascharles@gmail.com>
2023-12-15 14:16:31 -0500

    Signup screens completed with validation

--- src/components/Auth/signUpComp.tsx
+++ src/components/Auth/signUpComp.tsx
@@ -5,10 +5,11 @@ import {
   Image,
   TextInput,
   TouchableOpacity,
-    Platform,
-  ScrollView
+  Platform,
+  ScrollView,
+  Alert,
 } from "react-native";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { Link, Stack, router } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { RFValue } from "react-native-responsive-fontsize";
@@ -17,10 +18,19 @@ import { Formik, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import InputField from "../inputs/inputField";
 import PasswordInputField from "../inputs/passwordInputField";
+import PhoneInput from "react-native-phone-number-input";
 import ErrorMsg from "./errors/errorMsg";
+import { CheckBox } from "@rneui/themed";
 
 export default function SignUpComp() {
   const [passwordVisible, setPasswordVisible] = useState(true);
+  const [value, setValue] = useState("");
+  const [formattedValue, setFormattedValue] = useState("");
+  const [phoneNumber, setphoneNumber] = useState("");
+  const [checked, setChecked] = React.useState(true);
+
+  const toggleCheckbox = () => setChecked(!checked);
+  const phoneInput = useRef(null);
   const handlePasswordVisibility = () => {
     setPasswordVisible(!passwordVisible);
   };
@@ -41,18 +51,21 @@ export default function SignUpComp() {
         }}
       />
       <StatusBar style="dark" />
-          <ScrollView
-              showsHorizontalScrollIndicator={false}
-              style={styles.container}>
+      <ScrollView
+        showsHorizontalScrollIndicator={false}
+        style={styles.container}
+      >
         <Formik
           initialValues={{
             fullName: "",
             email: "",
+            phone: "",
             state: "",
             city: "",
             address: "",
-            confirmPassword: "",
             password: "",
+            confirmPassword: "",
+            checkBox: "",
           }}
           validationSchema={Yup.object({
             fullName: Yup.string().required("Full name is required").min(5),
@@ -70,6 +83,8 @@ export default function SignUpComp() {
               [Yup.ref("password")],
               "Passwords must match"
             ),
+            phone: Yup.number().required("A phone number is required"),
+            checkBox: Yup.boolean().required("This field is required"),
           })}
           onSubmit={async (values: any, { setSubmitting }) =>
             handleUserLogin(values, setSubmitting)
@@ -112,6 +127,39 @@ export default function SignUpComp() {
                   errorMessage={errors.email}
                   placeholderTextColor={"#5f5f5f"}
                 />
+                <Text
+                  style={{
+                    fontFamily: "outfit-bold",
+                    fontSize: RFValue(14),
+                    marginTop: RFValue(15),
+                    color: "#5f5f5f",
+                  }}
+                >
+                  Phone number
+                </Text>
+                <View
+                  style={{
+                    borderRadius: 10,
+                    borderWidth: 1,
+                    borderColor: "#E4E4E7",
+                    padding: RFValue(8),
+                    marginTop: RFValue(10),
+                  }}
+                >
+                  <PhoneInput
+                    placeholder="643-334-0009"
+                    ref={phoneInput}
+                    defaultValue={value}
+                    onChangeText={handleChange("phone")}
+                    defaultCode="NG"
+                    layout="first"
+                    onChangeFormattedText={(text) => {
+                      setFormattedValue(text);
+                    }}
+                    textContainerStyle={styles.textInput}
+                  />
+                </View>
+                {errors.phone && <ErrorMsg message={`${errors.phone}`} />}
                 <InputField
                   label={"State"}
                   value={values.state}
@@ -170,14 +218,49 @@ export default function SignUpComp() {
                   returnKeyType="done"
                 />
               </View>
-
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  width: 350,
+                  marginLeft: RFValue(-18),
+                }}
+              >
+                <CheckBox
+                  checked={checked}
+                  onPress={toggleCheckbox}
+                  iconType="material-community"
+                  checkedIcon="checkbox-marked"
+                  uncheckedIcon="checkbox-blank-outline"
+                  checkedColor="#06782f"
+                  size={30}
+                />
+                <Text style={styles.TStext}>
+                  I agree to the company{" "}
+                  <Link href={"/auth/login"} asChild>
+                    <Text style={{ color: "#06782f" }}>Term of Service</Text>
+                  </Link>{" "}
+                  and{" "}
+                  <Link href={"/auth/login"} asChild>
+                    <Text style={{ color: "#06782f" }}>Privacy Policy</Text>
+                  </Link>
+                </Text>
+              </View>
               <View>
-                {isSubmitting || errors.email || errors.password ? (
+                {isSubmitting ||
+                errors.fullName ||
+                errors.email ||
+                errors.phone ||
+                errors.state ||
+                errors.city ||
+                errors.address ||
+                errors.password ||
+                errors.confirmPassword ||
+                errors.checkBox ? (
                   <TouchableOpacity style={styles.disableBtn}>
                     <Text style={styles.button}>Sign in</Text>
                   </TouchableOpacity>
                 ) : (
-                  // <Link href={"/auth/otpverification"} asChild>
                   <TouchableOpacity
                     onPress={() => handleSubmit()}
                     style={styles.activeBtn}
@@ -319,7 +402,7 @@ const styles = StyleSheet.create({
   },
   activeBtn: {
     backgroundColor: "#06782f",
-    marginTop: RFValue(10),
+    marginTop: RFValue(20),
     paddingHorizontal: RFValue(14),
     paddingVertical: RFValue(12),
     borderRadius: Platform.OS == "android" ? 15 : 15,
@@ -372,5 +455,29 @@ const styles = StyleSheet.create({
     paddingVertical: RFValue(5),
     width: RFValue(140),
     borderRadius: RFValue(5),
+  },
+  textInput: {
+    paddingVertical: 0,
+    color: "red",
+    backgroundColor: "#fff",
+    borderLeft: 1,
+    borderLeftColor: "#000",
+  },
+  phoneContainer: {
+    width: "75%",
+    height: 50,
+  },
+  buttonProp: {
+    marginTop: 30,
+    width: "75%",
+    padding: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "green",
+  },
+  TStext: {
+    fontSize: RFValue(13),
+    fontFamily: "plusjakarta-regular",
+    marginLeft: RFValue(-10),
   },
 });
