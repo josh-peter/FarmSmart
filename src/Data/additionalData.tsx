c164b24d6d3abb27f20ee01a34d876dd735d1bfd
T-e-y-e <teyeetukudo@gmail.com>
2024-03-13 22:32:10 +0100

    Updates on account screen

--- src/Data/additionalData.tsx
+++ src/Data/additionalData.tsx
@@ -1,19 +1,19 @@
 export const additionalData = [
   {
     id: 1,
-    img: require("../assets/images/help.png"),
+    img: require("../assets/images/24-support.png"),
     info: "Help center",
     entails: "FAQs, Contact support",
   },
   {
     id: 2,
-    img: require("../assets/images/privacy.png"),
+    img: require("../assets/images/security-safe.png"),
     info: "Privacy Policy",
     entails: "Terms and condition",
   },
   {
     id: 3,
-    img: require("../assets/images/rate.png"),
+    img: require("../assets/images/medal-star.png"),
     info: "Rate App",
     entails: "Help us serve you better",
   },
--- src/app/home/account.tsx
+++ src/app/home/account.tsx
@@ -14,11 +14,17 @@ import { additionalData } from "../../Data/additionalData";
 import { accountData } from "../../Data/accountData";
 import { Stack, router } from "expo-router";
 import { Switch } from "react-native-switch";
+import colors from "../../constants/Colors";
 const { width, height } = Dimensions.get("window");
 
 export default function Account() {
    const [animationTriggered, setAnimationTriggered] = useState(false);
-   const [fade] = useState(new Animated.Value(0));
+  const [fade] = useState(new Animated.Value(0));
+  const [toggleLocation, setToggleLocation] = useState(false);
+
+  const handleToggleLocation = () => {
+    setToggleLocation((prev) => !prev);
+  };
 
   const animation = () => {
     Animated.timing(fade, {
@@ -63,64 +69,55 @@ export default function Account() {
                 }),
               },
             ],
-          }}
-        >
+          }}>
           <View>
-            <Text
-              style={{
-                fontSize: RFValue(15),
-                fontFamily: "outfit-bold",
-                lineHeight: RFValue(40),
-                color: "#BABBBC",
-              }}
-            >
-              Profile
-            </Text>
             <TouchableOpacity
               onPress={() => router.push("/client-profile")}
               style={{
                 flexDirection: "row",
                 alignItems: "center",
                 justifyContent: "space-between",
-                backgroundColor: "#f9fafa",
+                backgroundColor: "#fff",
                 padding: RFValue(10),
                 borderRadius: 10,
-              }}
-            >
+                borderWidth: 1,
+                borderColor: "#f3f4f6",
+                shadowOffset: { width: 2, height: 2 },
+                shadowRadius: 20,
+                elevation: 2,
+                shadowColor: "#d1d5db",
+              }}>
               <View
                 style={{
                   flexDirection: "row",
                   alignItems: "center",
                   gap: RFValue(8),
-                }}
-              >
+                }}>
                 <Image
                   resizeMode="contain"
                   source={require("../../assets/images/userProfile.png")}
                   style={{
-                    height: RFValue(60),
+                    height: RFValue(50),
                     width: RFValue(50),
                   }}
                 />
                 <View>
                   <Text
                     style={{
-                      fontSize: RFValue(14),
-                      fontFamily: "plusjakarta-semibold",
+                      fontSize: RFValue(15),
+                      fontFamily: "outfit-semibold",
                       lineHeight: RFValue(20),
                       color: "#1A1A1A",
-                    }}
-                  >
+                    }}>
                     Daniel Israel
                   </Text>
                   <Text
                     style={{
                       fontSize: RFValue(12),
-                      fontFamily: "plusjakarta-regular",
+                      fontFamily: "urbanist-medium",
                       lineHeight: RFValue(20),
                       color: "#1A1A1A",
-                    }}
-                  >
+                    }}>
                     Show profile
                   </Text>
                 </View>
@@ -140,78 +137,90 @@ export default function Account() {
               <View
                 key={item.id}
                 style={{
-                  marginTop: RFValue(10),
-                }}
-              >
-                <Text
-                  style={{
-                    fontSize: RFValue(15),
-                    fontFamily: "outfit-bold",
-                    lineHeight: RFValue(40),
-                    color: "#BABBBC",
-                  }}
-                >
-                  {item.name}
-                </Text>
+                  marginTop: RFValue(18),
+                }}>
                 <TouchableOpacity
                   onPress={() =>
                     item.id === 1
-                      ? router.push("/account-information")
+                      ? router.push("/account-information/")
                       : item.id === 2
-                        ? router.push("/booking")
-                        : item.id === 3
-                          ? router.push("/payment-management")
-                          : item.id === 4
-                            ? router.push("/notification-settings")
-                            : item.id === 5
-                              ? router.push("/security")
-                              : item.id === 6
-                                ? router.push("/help-center")
-                                : undefined
+                      ? router.push("/booking")
+                      : item.id === 3
+                      ? router.push("/payment-management/")
+                      : item.id === 4
+                      ? router.push("/notification-settings")
+                      : item.id === 5
+                      ? router.push("/security")
+                      : item.id === 6
+                      ? router.push("/help-center")
+                      : undefined
                   }
                   style={{
                     flexDirection: "row",
                     alignItems: "center",
                     justifyContent: "space-between",
-                    backgroundColor: "#f9fafa",
+                    backgroundColor: "#fff",
                     padding: RFValue(10),
                     borderRadius: 10,
-                  }}
-                >
+                    borderWidth: 1,
+                    borderColor: "#f3f4f6",
+                    shadowOffset: { width: 2, height: 2 },
+                    shadowRadius: 20,
+                    elevation: 2,
+                    shadowColor: "#d1d5db",
+                  }}>
                   <View
                     style={{
                       flexDirection: "row",
                       alignItems: "center",
                       gap: RFValue(8),
-                    }}
-                  >
-                    <Image
-                      resizeMode="contain"
-                      source={item.img}
+                    }}>
+                    <View
                       style={{
-                        height: RFValue(60),
-                        width: RFValue(50),
-                      }}
-                    />
-                    <View>
+                        flexDirection: "row",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        borderWidth: 1,
+                        borderColor: "#f3f4f6",
+                        backgroundColor: "#fff",
+                        padding: RFValue(5),
+                        borderRadius: 50,
+                        height: RFValue(40),
+                        width: RFValue(40),
+                        shadowOffset: { width: 2, height: 2 },
+                        shadowRadius: 20,
+                        elevation: 2,
+                        shadowColor: "#d1d5db",
+                      }}>
+                      <Image
+                        resizeMode="contain"
+                        source={item.img}
+                        style={{
+                          height: RFValue(18),
+                          width: RFValue(18),
+                        }}
+                      />
+                    </View>
+                    <View
+                      style={{
+                        maxWidth: RFValue(200),
+                      }}>
                       <Text
                         style={{
-                          fontSize: RFValue(12),
-                          fontFamily: "plusjakarta-semibold",
+                          fontSize: RFValue(14),
+                          fontFamily: "outfit-semibold",
                           lineHeight: RFValue(20),
                           color: "#1A1A1A",
-                        }}
-                      >
+                        }}>
                         {item.info}
                       </Text>
                       <Text
                         style={{
-                          fontSize: RFValue(10),
-                          fontFamily: "plusjakarta-regular",
+                          fontSize: RFValue(12),
+                          fontFamily: "urbanist-medium",
                           lineHeight: RFValue(20),
                           color: "#1A1A1A",
-                        }}
-                      >
+                        }}>
                         {item.entails}
                       </Text>
                     </View>
@@ -228,33 +237,34 @@ export default function Account() {
               </View>
             ))}
           </View>
-          <View>
+          <View
+            style={{
+              marginTop: RFValue(15),
+              marginBottom: RFValue(10),
+            }}>
             <Text
               style={{
-                fontSize: RFValue(15),
+                fontSize: RFValue(14),
                 fontFamily: "outfit-bold",
                 lineHeight: RFValue(40),
                 color: "#BABBBC",
-              }}
-            >
+              }}>
               Location
             </Text>
-            <TouchableOpacity
+            <View
               style={{
                 backgroundColor: "#f9fafa",
                 padding: RFValue(10),
                 borderRadius: 10,
-              }}
-            >
+              }}>
               <View>
                 <Text
                   style={{
                     fontSize: RFValue(13),
-                    fontFamily: "plusjakarta-regular",
+                    fontFamily: "urbanist-medium",
                     lineHeight: RFValue(20),
                     color: "#1A1A1A",
-                  }}
-                >
+                  }}>
                   Give location to get nearby property suggestion
                 </Text>
                 <View
@@ -263,30 +273,28 @@ export default function Account() {
                     alignItems: "center",
                     justifyContent: "space-between",
                     marginTop: RFValue(10),
-                  }}
-                >
+                  }}>
                   <Text
                     style={{
-                      fontSize: RFValue(12),
-                      fontFamily: "plusjakarta-regular",
+                      fontSize: RFValue(15),
+                      fontFamily: "outfit-semibold",
                       lineHeight: RFValue(20),
                       color: "#1A1A1A",
-                    }}
-                  >
+                    }}>
                     Grant location access
                   </Text>
                   <Switch
-                    value={true}
-                    onValueChange={(val) => console.warn(val)}
-                    backgroundActive={"green"}
-                    backgroundInactive={"gray"}
+                    value={toggleLocation}
+                    onValueChange={handleToggleLocation}
+                    backgroundActive={colors.primary}
+                    backgroundInactive={"#E4E4E7"}
                     activeText={""}
                     inActiveText={""}
                     circleSize={25}
                   />
                 </View>
               </View>
-            </TouchableOpacity>
+            </View>
           </View>
           <View>
             {additionalData?.map((item) => (
@@ -294,59 +302,81 @@ export default function Account() {
                 key={item.id}
                 style={{
                   marginTop: RFValue(20),
-                }}
-              >
+                }}>
                 <TouchableOpacity
                   onPress={() =>
                     item.id === 1
                       ? router.push("/help-center")
                       : item.id === 2
-                        ? router.push("/")
-                        : undefined
+                      ? router.push("/")
+                      : undefined
                   }
                   style={{
                     flexDirection: "row",
                     alignItems: "center",
                     justifyContent: "space-between",
-                    backgroundColor: "#f9fafa",
+                    backgroundColor: "#fff",
                     padding: RFValue(10),
                     borderRadius: 10,
-                  }}
-                >
+                    borderWidth: 1,
+                    borderColor: "#f3f4f6",
+                    shadowOffset: { width: 2, height: 2 },
+                    shadowRadius: 20,
+                    elevation: 2,
+                    shadowColor: "#d1d5db",
+                  }}>
                   <View
                     style={{
                       flexDirection: "row",
                       alignItems: "center",
                       gap: RFValue(8),
-                    }}
-                  >
-                    <Image
-                      resizeMode="contain"
-                      source={item.img}
+                    }}>
+                    <View
                       style={{
-                        height: RFValue(60),
-                        width: RFValue(50),
-                      }}
-                    />
-                    <View>
+                        flexDirection: "row",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        borderWidth: 1,
+                        borderColor: "#f3f4f6",
+                        backgroundColor: "#fff",
+                        padding: RFValue(5),
+                        borderRadius: 50,
+                        height: RFValue(40),
+                        width: RFValue(40),
+                        shadowOffset: { width: 2, height: 2 },
+                        shadowRadius: 20,
+                        elevation: 2,
+                        shadowColor: "#d1d5db",
+                      }}>
+                      <Image
+                        resizeMode="contain"
+                        source={item.img}
+                        style={{
+                          height: RFValue(18),
+                          width: RFValue(18),
+                        }}
+                      />
+                    </View>
+                    <View
+                      style={{
+                        maxWidth: RFValue(200),
+                      }}>
                       <Text
                         style={{
-                          fontSize: RFValue(12),
-                          fontFamily: "plusjakarta-semibold",
+                          fontSize: RFValue(14),
+                          fontFamily: "outfit-semibold",
                           lineHeight: RFValue(20),
                           color: "#1A1A1A",
-                        }}
-                      >
+                        }}>
                         {item.info}
                       </Text>
                       <Text
                         style={{
-                          fontSize: RFValue(10),
-                          fontFamily: "plusjakarta-regular",
+                          fontSize: RFValue(12),
+                          fontFamily: "urbanist-medium",
                           lineHeight: RFValue(20),
                           color: "#1A1A1A",
-                        }}
-                      >
+                        }}>
                         {item.entails}
                       </Text>
                     </View>
@@ -366,42 +396,62 @@ export default function Account() {
           <View
             style={{
               marginTop: RFValue(20),
-            }}
-          >
+            }}>
             <TouchableOpacity
               style={{
                 flexDirection: "row",
                 alignItems: "center",
                 justifyContent: "space-between",
-                backgroundColor: "#f9fafa",
+                backgroundColor: "#fff",
                 padding: RFValue(10),
                 borderRadius: 10,
-              }}
-            >
+                borderWidth: 1,
+                borderColor: "#f3f4f6",
+                shadowOffset: { width: 2, height: 2 },
+                shadowRadius: 20,
+                elevation: 2,
+                shadowColor: "#d1d5db",
+              }}>
               <View
                 style={{
                   flexDirection: "row",
                   alignItems: "center",
                   gap: RFValue(8),
-                }}
-              >
-                <Image
-                  resizeMode="contain"
-                  source={require("../../assets/images/logout.png")}
+                }}>
+                <View
                   style={{
-                    height: RFValue(60),
-                    width: RFValue(50),
-                  }}
-                />
+                    flexDirection: "row",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    borderWidth: 1,
+                    borderColor: "#f3f4f6",
+                    backgroundColor: "#fff",
+                    padding: RFValue(5),
+                    borderRadius: 50,
+                    height: RFValue(40),
+                    width: RFValue(40),
+                    shadowOffset: { width: 2, height: 2 },
+                    shadowRadius: 20,
+                    elevation: 2,
+                    shadowColor: "#d1d5db",
+                  }}>
+                  <Image
+                    resizeMode="contain"
+                    source={require("../../assets/images/account-logout.png")}
+                    style={{
+                      height: RFValue(20),
+                      width: RFValue(20),
+                    }}
+                  />
+                </View>
                 <View>
                   <Text
                     style={{
                       fontSize: RFValue(15),
-                      fontFamily: "plusjakarta-regular",
+                      fontFamily: "outfit-bold",
                       lineHeight: RFValue(20),
-                      color: "#1A1A1A",
-                    }}
-                  >
+                      color: "#FA5C47",
+                    }}>
                     Logout
                   </Text>
                 </View>
@@ -410,22 +460,23 @@ export default function Account() {
           </View>
           <TouchableOpacity
             style={{
-              backgroundColor: "#ECFFF4",
-              padding: Platform.OS === "ios" ? 18 : 17,
               borderRadius: 10,
-              marginTop: RFValue(35),
+              borderBottomRightRadius: 0,
+              borderWidth: RFValue(1),
+              borderColor: "#F0F4FF",
+              backgroundColor: "#FDFDFD",
+              padding: Platform.OS === "ios" ? 18 : 17,
+              marginTop: RFValue(24),
               marginBottom: RFValue(105),
-            }}
-          >
+            }}>
             <Text
               style={{
-                fontSize: RFValue(16),
-                fontFamily: "outfit-regular",
-                color: "#06782F",
+                fontSize: RFValue(14),
+                color: colors.primary,
+                fontFamily: "outfit-semibold",
                 textAlign: "center",
-              }}
-            >
-              Switch to agent account mode
+              }}>
+              Become an Easyfynd agent
             </Text>
           </TouchableOpacity>
         </Animated.ScrollView>
